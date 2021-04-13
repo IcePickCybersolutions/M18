55dd5185f37b75a4ee2df82afbce59a1e5c55527
IcePickCyberSolutions <80115033+IcePickCybersolutions@users.noreply.github.com>
2021-04-13 10:03:32 -0600

    Add files via upload

---
+++ m18.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function m18() {
+	IPINITIAL=$(hostname --ip-address)
+	echo "Initial IP address is $IPINITIAL"
+	IPWPROXY=$(proxychains hostname --ip-address)
+	echo "Proxied IP address is $IPWPROXY"
+	nmcli con up id ConnectionName
+	IPWVPN=$(honstname --ip-address)
+	if [$IPINITIAL == $IPWPROXY -a $IPINITIAL == $IPWVPN]
+	then
+		echo "Proxies and VPNs ineffective, recofigure proxychains and VPN connection"
+	elif [$IPINITIAL == $IPWPROXY -a $IPINITIAL != $IPWVPN]
+	then
+		echo "VPN active, proxies innefective, recofigure proxychains"
+	elif [$IPINITIAL != $IPWPROXY -a $ IPINITIAL == $IPWVPN]
+	then
+		echo "proxies active, VPN innefective, check VPN connection"
+	else 
+		echo "Proxies and VPN active, good to go cap'n"
+	fi
